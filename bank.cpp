4e95854e8df476f0b1641e4c0fdd3a54b9587bb1
Stephen <75145009+kingsteph23@users.noreply.github.com>
2024-01-08 00:32:56 -0500

    Create bank.cpp

---
+++ bank.cpp
@@ -0,0 +1,77 @@
+include <iostream>
+using namespace std;
+#include <iomanip>
+#include <cstdlib> 
+#include <time.h>
+
+//global variables
+double initbalance; //inital balance for savings/checking
+double balance; //checking balance
+std::string firstname; //firstname of user
+std::string lastname; //lastname of user
+double savingsbalance; //savings balance
+//savings
+void savings()
+{
+//ask users name
+  //last name
+  //inital balance
+  //display name/account type/current balance for said accoutn
+}
+//checking
+void checking()
+{
+//ask users name
+  //last name
+  //inital balance
+  //display name/account type/current balance for said accoutn
+}
+
+// deposit
+double depo()
+{
+//deposit for either check or savings
+  //repeat this deposit until user says no
+  //show current balance once ended
+}
+
+
+//withdrawal
+double withdraw()
+{
+//withdrawal for either check or savings
+  //repeat this withdrawal until user says no
+  //show current balance once ended
+}
+
+//display(balance inquiry/account(s) summary)
+void display()
+{
+//display account summary of both savings/checkings
+}
+
+
+/transferring money between accounts
+void transfer()
+{
+  //amount to trnsfer
+  //find a way to update savings and/or checkings balances.
+
+
+}
+//opening an account
+void openaccount() {
+//limit of two accounts
+  //check  with for loop(increment)
+  //once you opened two accounts, break through loop. SHow message that you have hit your limit(2 accounts max)
+}
+//main fuction
+int main(){
+  //options(open an account,deposit,withdrawal,balance inquiry,transfer,exit)
+  //based on user input, call corresponding function
+  //repeat, until user enters exit(or its equivlanet)
+  // end message-  "Thank you for banking at Manhattan College Banking and Co!"
+
+}
+
+
